a460ecc5168c10814671a3df3a4cea6253d4da78
Zeugma440 <Zeugma440@users.noreply.github.com>
2024-08-10 22:33:00 +0200

    Cleanup

--- SpawnDev.EBML/Matroska/MatroskaSchema.cs
+++ SpawnDev.EBML/Matroska/MatroskaSchema.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace SpawnDev.EBML.Matroska
+﻿namespace SpawnDev.EBML.Matroska
 {
     public class MatroskaSchema : EBMLSchema<MatroskaId>
     {
