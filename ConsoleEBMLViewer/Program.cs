c90f5d526508b12384ef2af5788f155f60d03d68
Todd Tanner <todd@spawndev.com>
2024-08-21 10:32:40 -0400

    README.md update

--- ConsoleEBMLViewer/Program.cs
+++ ConsoleEBMLViewer/Program.cs
@@ -2,18 +2,18 @@
 
 // Create the EBML parser with default configuration
 // default configuration supports matroska and webm reading and modification
-var ebmlParser = new EBMLParser();
+var ebml = new EBMLParser();
 // get a stream containing an EBML document (or multiple documents)
 using var fileStream = File.Open(@"TestData\Big_Buck_Bunny_180 10s.webm", FileMode.Open);
 // parse the EBML document stream (ParseDocuments can be used to parse all documents in the stream)
-var document = ebmlParser.ParseDocument(fileStream);
+var document = ebml.ParseDocument(fileStream);
 if (document != null)
 {
     Console.WriteLine($"DocType: {document.DocType}");
 }
 
 // Create a new matroska EBML file
-var matroskaDoc = ebmlParser.CreateDocument("matroska");
+var matroskaDoc = ebml.CreateDocument("matroska");
 Console.WriteLine($"DocType: {matroskaDoc.DocType}");
 
 // ...
